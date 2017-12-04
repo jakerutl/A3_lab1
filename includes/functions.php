e3487642ebb357e57c0d38ab3913a368f87d841e
jakerutl <j_rutledge3@fanshaweonline.ca>
2017-12-04 17:22:25 -0500

    implamented AJAX

---
+++ includes/functions.php
@@ -0,0 +1,42 @@
+<?php
+  $user = "root";
+  $pass = "root"; //this should be a blank string for windows machines
+  $host = "localhost";
+  $db = "db_cooperinfo"; // whatever you called your database
+
+  $conn = mysqli_connect($host, $user, $pass, $db);
+  mysqli_set_charset('utf8');
+
+  if (!$conn){
+    echo "something broke... no soup for you!";
+    exit;
+  }
+
+// echo "connected!";
+
+
+$myQuery = "SELECT * FROM mainmodel";
+$result = mysqli_query($conn, $myQuery);
+
+$rows = array();
+
+while($row = mysqli_fetch_assoc($result)) {
+  $rows[] = $row;
+}
+
+// var_dump($rows);
+// echo json_encode($rows);
+
+//get a single row (one result) using a query parameter
+if (isset($_GET['carModel'])){//see if there's a parameter called carModel
+  $car = $_GET['carModel']; // whatever comes after the ?
+
+  $myQuery = "SELECT * FROM mainmodel WHERE model = '$car'";
+  $result = mysqli_query($conn, $myQuery);
+
+  $row = mysqli_fetch_assoc($result);
+
+  echo json_encode($row);
+}
+
+ ?>
--- js/main.js
+++ js/main.js
@@ -1,39 +1,59 @@
 (function () {
   // start with retrieving the elements from the page, and then adding event handling. then write the logic. refer to the seasons example / homework
 var images = document.querySelectorAll('.data-ref');
-var name = document.querySelector('.modelName');
-var  price = document.querySelector('.priceInfo');
-var  details = document.querySelector('.modelDetails');
-var  current = document.querySelector('#F55');
-    // appliedClass;
+var httpRequest = new XMLHttpRequest();
 
 
 
-    function changeText(){
-      current.classList.add('nonActive');
-      // console.log('hello');
-      let objectIndex = carData[this.id];
-      // console.log(objectIndex);
-      this.classList.remove('nonActive');
-      name.firstChild.nodeValue = objectIndex.carName;
-      price.firstChild.nodeValue = objectIndex.price;
-      details.firstChild.nodeValue = objectIndex.imageDescription;
 
-      current = this;
+    function getCarData(){
+//make an AJAX call to the DB; handle errors first
+  if (!httpRequest){
+    alert('giving up... your browser sucks');
+    return false;
+  }
 
+  httpRequest.onreadystatechange = processRequest;
+  httpRequest.open('Get', './includes/functions.php?carModel=' + this.id);
+  httpRequest.send();
+
+}
+
+function processRequest() {
+  let reqIndicator = document.querySelector('.request-state');
+  reqIndicator.textContent = httpRequest.readyState;
+  // debugger;
+
+  if (httpRequest.readyState === XMLHttpRequest.DONE) {
+    if (httpRequest.status === 200) { // 200 means everything is awesome
+      // debugger;
+      let data = JSON.parse(httpRequest.responseText);
+
+      processResult(data);
+    } else {
+      alert('There was a problem with the request.');
+    }
+  }
+}
+
+
+
+    function processResult(data) {
+      const { modelName, pricing, modelDetails } = data;
+
+      let  model = document.querySelector('.modelName').textContent = modelName;
+      let  price = document.querySelector('.priceInfo').innerHTML = pricing;
+      let  desc = document.querySelector('.modelDetails').textContent = modelDetails;
+
+      carButtons.forEach(function(car,index){
+        car.classList.add('nonActive');
+      });
+
+      // this is a template string constructor - look it up!
+      document.querySelector('#${data.model}').classList.remove('nonActive');
     }
 
-    images.forEach(function(image){
-      // console.log(image);
-      image.addEventListener('click', changeText, false);
-      image.classList.add('nonActive');
-
-
-    });
-
-current.classList.remove('nonActive');
-let objectIndex = carData[current.id];
-name.firstChild.nodeValue = objectIndex.carName;
-price.firstChild.nodeValue = objectIndex.price;
-details.firstChild.nodeValue = objectIndex.imageDescription;
+images.forEach(function(car, index) {
+  car.addEventListener('click', getCarData, false);
+  });
 })();
