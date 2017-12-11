0fb0974cf46997c7bffa757651474fca58b03c34
jakerutl <j_rutledge3@fanshaweonline.ca>
2017-12-11 16:10:19 -0500

    changed so there is even less code

--- js/main.js
+++ js/main.js
@@ -12,56 +12,39 @@ var images = document.querySelectorAll('.data-ref');
       //the fetch API uses new Javascript promise API
       fetch(url)// do an ajax call with fetch
         .then((resp) => resp.json())// convert to json
-        .then((data) => { processResult(data); }) //call the process function
-        .catch(function(error){
+        .then(({modelName, pricing, modelDetails, model}) => {
+          let  carmodel = document.querySelector('.modelName').textContent = modelName;
+          let  price = document.querySelector('.priceInfo').innerHTML = pricing;
+          let  desc = document.querySelector('.modelDetails').textContent = modelDetails;
+
+        images.forEach(function(image, index){
+            image.classList.add('nonActive');
+          });
+
+          // this is a template string constructor - look it up!
+          document.querySelector(`#${model}`).classList.remove('nonActive');
+        }) //call the process function
+        .catch(function(error){ // catch ANY error
           console.log(error);
         })
-
-//make an AJAX call to the DB; handle errors first
-  // if (!httpRequest){
-  //   alert('giving up... your browser sucks');
-  //   return false;
-  // }
-  //
-  // httpRequest.onreadystatechange = processRequest;
-  // httpRequest.open('Get', './includes/functions.php?carModel=' + this.id);
-  // httpRequest.send();
-//
 }
 
-// function processRequest() {
-//   let reqIndicator = document.querySelector('.request-state');
-//   reqIndicator.textContent = httpRequest.readyState;
-//   // debugger;
-//
-//   if (httpRequest.readyState === XMLHttpRequest.DONE) {
-//     if (httpRequest.status === 200) { // 200 means everything is awesome
-//       // debugger;
-//       let data = JSON.parse(httpRequest.responseText);
-//
-//       processResult(data);
-//     } else {
-//       alert('There was a problem with the request.');
-//     }
-//   }
-// }
 
-
-
-    function processResult(data) {
-      const { modelName, pricing, modelDetails } = data;
-
-      let  model = document.querySelector('.modelName').textContent = modelName;
-      let  price = document.querySelector('.priceInfo').innerHTML = pricing;
-      let  desc = document.querySelector('.modelDetails').textContent = modelDetails;
-
-    images.forEach(function(image, index){
-        image.classList.add('nonActive');
-      });
-
-      // this is a template string constructor - look it up!
-      document.querySelector(`#${data.model}`).classList.remove('nonActive');
-    }
+    //
+    // function processResult(data) {
+    //   const { modelName, pricing, modelDetails } = data;
+    //
+    //   let  model = document.querySelector('.modelName').textContent = modelName;
+    //   let  price = document.querySelector('.priceInfo').innerHTML = pricing;
+    //   let  desc = document.querySelector('.modelDetails').textContent = modelDetails;
+    //
+    // images.forEach(function(image, index){
+    //     image.classList.add('nonActive');
+    //   });
+    //
+    //   // this is a template string constructor - look it up!
+    //   document.querySelector(`#${data.model}`).classList.remove('nonActive');
+    // }
 
 images.forEach(function(car, index) {
   car.addEventListener('click', getCarData, false);
