f57f68fdd13659da02c807f420e322b0b250d1d0
jakerutl <j_rutledge3@fanshaweonline.ca>
2017-12-11 15:52:00 -0500

    cut the php in half while still getting the same result

--- js/main.js
+++ js/main.js
@@ -1,40 +1,50 @@
 (function () {
   // start with retrieving the elements from the page, and then adding event handling. then write the logic. refer to the seasons example / homework
 var images = document.querySelectorAll('.data-ref');
-var httpRequest = new XMLHttpRequest();
+// var httpRequest = new XMLHttpRequest();
 
 
 
 
     function getCarData(){
+      const url = './includes/functions.php?carModel=' + this.id;
+
+      //the fetch API uses new Javascript promise API
+      fetch(url)// do an ajax call with fetch
+        .then((resp) => resp.json())// convert to json
+        .then((data) => { processResult(data); }) //call the process function
+        .catch(function(error){
+          console.log(error);
+        })
+
 //make an AJAX call to the DB; handle errors first
-  if (!httpRequest){
-    alert('giving up... your browser sucks');
-    return false;
-  }
-
-  httpRequest.onreadystatechange = processRequest;
-  httpRequest.open('Get', './includes/functions.php?carModel=' + this.id);
-  httpRequest.send();
-
+  // if (!httpRequest){
+  //   alert('giving up... your browser sucks');
+  //   return false;
+  // }
+  //
+  // httpRequest.onreadystatechange = processRequest;
+  // httpRequest.open('Get', './includes/functions.php?carModel=' + this.id);
+  // httpRequest.send();
+//
 }
 
-function processRequest() {
-  let reqIndicator = document.querySelector('.request-state');
-  reqIndicator.textContent = httpRequest.readyState;
-  // debugger;
-
-  if (httpRequest.readyState === XMLHttpRequest.DONE) {
-    if (httpRequest.status === 200) { // 200 means everything is awesome
-      // debugger;
-      let data = JSON.parse(httpRequest.responseText);
-
-      processResult(data);
-    } else {
-      alert('There was a problem with the request.');
-    }
-  }
-}
+// function processRequest() {
+//   let reqIndicator = document.querySelector('.request-state');
+//   reqIndicator.textContent = httpRequest.readyState;
+//   // debugger;
+//
+//   if (httpRequest.readyState === XMLHttpRequest.DONE) {
+//     if (httpRequest.status === 200) { // 200 means everything is awesome
+//       // debugger;
+//       let data = JSON.parse(httpRequest.responseText);
+//
+//       processResult(data);
+//     } else {
+//       alert('There was a problem with the request.');
+//     }
+//   }
+// }
 
 
 
